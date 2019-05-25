8adbfefc272c523e99b2b105c2bb8bf2d5075e13
Maged Mohammed <maged.mo.it@gmail.com>
2019-05-25 23:13:57 +0200

    Initial Commit

---
+++ MVCtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCtest
+//
+//  Created by Maged on 5/25/19.
+//  Copyright © 2019 Be Solutions - Maged Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
